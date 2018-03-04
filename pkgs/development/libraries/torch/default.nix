3752037569d81a5a93ccc43151d6a4f1305b8c45
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2018-03-04 21:35:04 +0100

    cutorch - my version (working normally)

--- pkgs/development/libraries/torch/default.nix
+++ pkgs/development/libraries/torch/default.nix
@@ -1,6 +1,7 @@
 {stdenv, fetchgit, luajit, openblas, imagemagick, cmake, curl, fftw, gnuplot
   , libjpeg, zeromq3, ncurses, openssl, libpng, qt4, readline, unzip
-  , pkgconfig, zlib, libX11, which, fetchFromGitHub, git, hdf5, cudatoolkit
+  , pkgconfig, zlib, libX11, which, fetchFromGitHub, git, hdf5
+  , cudatoolkit, gcc5
   }:
 stdenv.mkDerivation rec{
   version = "0.0pre20160820";
@@ -8,13 +9,13 @@ stdenv.mkDerivation rec{
   buildInputs = [
     luajit openblas imagemagick cmake curl fftw gnuplot unzip qt4
     libjpeg zeromq3 ncurses openssl libpng readline pkgconfig
-    zlib libX11 which git hdf5 cudatoolkit
+    zlib libX11 which git hdf5 cudatoolkit gcc5
   ];
 
   src = fetchgit {
     url = "https://github.com/torch/distro";
-    rev = "8b6a834f8c8755f6f5f84ef9d8da9cfc79c5ce1f";
-    sha256 = "120hnz82d7izinsmv5smyqww71dhpix23pm43s522dfcglpql8xy";
+    rev = "0219027e6c4644a0ba5c5bf137c989a0a8c9e01b";
+    sha256 = "1xcy6q1pkhzrrr44yc35wq53lwz09mvxk0fhcwjhjp671lzn98x6";
     fetchSubmodules = true;
   };
 
@@ -25,13 +26,6 @@ stdenv.mkDerivation rec{
     sha256 = "15cf2km3ky0wj3n6r0z28w90aysj21i5w003bar0xsq6a2r2171f";
   };
 
-  # unpackPhase = ''
-  #   tar -xzf $src --strip-components=1
-  #   tar -xzf $torch_hdf5_src --strip-components=1
-  # '';
-
-  tmpBuildDir = "/tmp/${name}";
-
   buildPhase = ''
     cd ..
     mkdir -p "$out"
@@ -39,18 +33,13 @@ stdenv.mkDerivation rec{
     mkdir -p $out/torch_hdf5
     cp -R $torch_hdf5_src/* $out/torch_hdf5
 
-    if [ -d ${tmpBuildDir} ]; then
-      echo 'Torch already compiled'
-    else
-      export PREFIX=${tmpBuildDir}
-      mkdir -p "${tmpBuildDir}"
-      sh install.sh -s
-    fi
-    cp -R -L ${tmpBuildDir}/* $out
+    export PREFIX=$out
+    sh install.sh -s
+
     export HOME=$TMP
-    $out/bin/luarocks install totem
-    $out/bin/luarocks install cutorch
     $out/bin/luarocks install cunn
+    $out/bin/luarocks install totem
+    # $out/bin/luarocks install cutorch
 
     cd $out/torch_hdf5
     $out/bin/luarocks make hdf5-0-0.rockspec
