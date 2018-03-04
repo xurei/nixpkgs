c6bd2f017ebcbadb2f5974e0651309acebf2caba
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2018-03-04 19:32:49 +0100

    Torch - my version

--- pkgs/development/libraries/torch/default.nix
+++ pkgs/development/libraries/torch/default.nix
@@ -1,6 +1,6 @@
 {stdenv, fetchgit, luajit, openblas, imagemagick, cmake, curl, fftw, gnuplot
   , libjpeg, zeromq3, ncurses, openssl, libpng, qt4, readline, unzip
-  , pkgconfig, zlib, libX11, which
+  , pkgconfig, zlib, libX11, which, fetchFromGitHub, git, hdf5
   }:
 stdenv.mkDerivation rec{
   version = "0.0pre20160820";
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec{
   buildInputs = [
     luajit openblas imagemagick cmake curl fftw gnuplot unzip qt4
     libjpeg zeromq3 ncurses openssl libpng readline pkgconfig
-    zlib libX11 which
+    zlib libX11 which git hdf5
   ];
 
   src = fetchgit {
@@ -18,15 +18,35 @@ stdenv.mkDerivation rec{
     fetchSubmodules = true;
   };
 
+  torch_hdf5_src = fetchFromGitHub {
+    owner = "deepmind";
+    repo = "torch-hdf5";
+    rev = "969ec7c5d004b8a5921168e80a845027a52ff123";
+    sha256 = "15cf2km3ky0wj3n6r0z28w90aysj21i5w003bar0xsq6a2r2171f";
+  };
+
+  # unpackPhase = ''
+  #   tar -xzf $src --strip-components=1
+  #   tar -xzf $torch_hdf5_src --strip-components=1
+  # '';
+
   buildPhase = ''
     cd ..
     export PREFIX=$out
+    mkdir -p "$out"
 
-    mkdir "$out"
-    sh install.sh -s
+    mkdir -p $out/torch_hdf5
+    cp -R $torch_hdf5_src/* $out/torch_hdf5
+
+    # sh install.sh -s
+    cp -R -L /home/olivier/nix/nixpkgs/distro-8b6a834/install/* $out
+    export HOME=$TMP
+    $out/bin/luarocks install totem
+
+    cd $out/torch_hdf5
+    $out/bin/luarocks make hdf5-0-0.rockspec
   '';
-  installPhase = ''
-  '';
+  installPhase = ":";
   meta = {
     inherit version;
     description = ''A scientific computing framework with wide support for machine learning algorithms'';
