32498a31e4a01a9105b76d0f4cb4d045df1fe183
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2018-04-02 01:33:19 +0200

    Torch config withcude

--- pkgs/development/libraries/torch/default.nix
+++ pkgs/development/libraries/torch/default.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchgit, luajit, openblas, imagemagick, cmake, curl, fftw, gnuplot
   , libjpeg, zeromq3, ncurses, openssl, libpng, qt4, readline, unzip
   , pkgconfig, zlib, libX11, which, fetchFromGitHub, git, hdf5
-  , cudatoolkit, gcc5
+  , cudatoolkit, gcc5, withcuda
   }:
 stdenv.mkDerivation rec{
   version = "0.0pre20160820";
@@ -9,8 +9,8 @@ stdenv.mkDerivation rec{
   buildInputs = [
     luajit openblas imagemagick cmake curl fftw gnuplot unzip qt4
     libjpeg zeromq3 ncurses openssl libpng readline pkgconfig
-    zlib libX11 which git hdf5 cudatoolkit gcc5
-  ];
+    zlib libX11 which git hdf5 gcc5
+  ] ++ (if withcuda then [ cudatoolkit ] else []);
 
   src = fetchgit {
     url = "https://github.com/torch/distro";
@@ -28,27 +28,25 @@ stdenv.mkDerivation rec{
 
   torchPath = "/home/olivier/torch";
 
+  configurePhase = ":";
+
+  buildCuda = if withcuda then "$out/bin/luarocks install cunn" else "";
+
   buildPhase = ''
-    cd ..
     mkdir -p "$out"
-    cp -R ${torchPath}/* $out
     mkdir -p $out/torch_hdf5
-    rm -R $out/bin/*
 
     export PREFIX=$out
     sh install.sh -s
 
     export HOME=$TMP
-    $out/bin/luarocks install cunn
+    $buildCuda
     $out/bin/luarocks install totem
     $out/bin/luarocks install rnn
-    # $out/bin/luarocks install cutorch
 
     cd $out/torch_hdf5
     cp -R $torch_hdf5_src/* $out/torch_hdf5
     $out/bin/luarocks make hdf5-0-0.rockspec
-
-    cp -R $out/* ${torchPath}
   '';
   installPhase = ":";
   meta = {
